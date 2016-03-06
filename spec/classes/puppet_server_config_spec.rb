f18695bf858cdcbc04b8a14b3d99333c8dcef068
Matt <matthew.a.nicholson@gmail.com>
2016-03-06 11:26:05 -0500

    fix regex

--- spec/classes/puppet_server_config_spec.rb
+++ spec/classes/puppet_server_config_spec.rb
@@ -107,7 +107,7 @@ describe 'puppet::server::config', :type => :class do
 
     context 'with autosign' do
       let(:pre_condition) { 'class { "puppet": server => true, autosign => ["*.test.domain"] }'}
-      it { should contain_file('/etc/puppetlabs/puppet/autosign.conf').with(:content => /'*.test.domain'/) }
+      it { should contain_file('/etc/puppetlabs/puppet/autosign.conf').with(:content => /\*\.test\.domain/) }
     end
 
     context 'with firewall' do
